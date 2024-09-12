e7dee9f4e0f22281766cfa4b72e5721c49717610
Alymuhammad Bijani <79768734+ambijani@users.noreply.github.com>
2024-09-12 16:43:24 -0500

    hotfix-09-update-buttons

--- clockin.py
+++ clockin.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 def main(position_line_number):
@@ -13,9 +14,13 @@ def main(position_line_number):
     try:
         with open(positions_file, 'r') as file:
             position_name = None
+            position_id = None
             for i, line in enumerate(file, start=1):
                 if i == position_line_number:
                     position_name = line.strip()
+                    position_id = i+5
+                    print(position_name)
+                    print("promptOption-gwt-uid-"+str(position_id))
                     break
             if position_name is None:
                 print(f"Line number {position_line_number} exceeds the number of lines in the file.")
@@ -27,6 +32,7 @@ def main(position_line_number):
     # Initialize the browser
     options = Options()
     browser = webdriver.Chrome(options)
+    actions = ActionChains(browser)
 
     login_url = 'https://one.wustl.edu/launch-task/all/workday'
     browser.get(login_url)
@@ -53,41 +59,45 @@ def main(position_line_number):
         print('Login success')
 
         # Clock-in button
-        checkin_button = WebDriverWait(browser, 10).until( EC.presence_of_element_located((By.CSS_SELECTOR, 'button.css-1yxsbie')) )
-        
-        dropdown = WebDriverWait(browser, 10).until(
-            EC.element_to_be_clickable((By.ID, "56$233036-input--uid12-input"))
+        checkin_button = WebDriverWait(browser, 30).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'button.css-1c4bazy'))
+        )
+        checkin_button.click()
+
+        # Select the dropdown
+        dropdown = WebDriverWait(browser, 30).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[@role='button' and @data-automation-id='selectWidget']"))
         )
         dropdown.click()
-
-        xpath_expression = f"//div[@aria-label='{position_name}']"
-
-        # Wait for the element to be clickable and then click on it
-        position_option = WebDriverWait(browser, 10).until(
-            EC.element_to_be_clickable((By.XPATH, xpath_expression))
+        
+        # Select the position from the dropdown
+        position_option = WebDriverWait(browser, 30).until(
+            EC.element_to_be_clickable((By.ID, "promptOption-gwt-uid-"+str(position_id)))
         )
-        browser.execute_script("arguments[0].scrollIntoView();", position_option) # scroll into element view
-        position_option.click()
+        browser.execute_script("arguments[0].scrollIntoView(true);", position_option)
+        time.sleep(1)
+        actions.move_to_element(position_option).click().perform()
+        browser.execute_script("arguments[0].scrollIntoView();", position_option)
 
-        ok_button = WebDriverWait(browser, 10).until(
-        EC.element_to_be_clickable((
-            By.XPATH, "//button[@data-automation-id='wd-CommandButton' and @title='OK']"
-        ))
+        # Click the OK button
+        ok_button = WebDriverWait(browser, 30).until(
+                EC.element_to_be_clickable((By.XPATH, "//button[@data-automation-id='wd-CommandButton' and @title='OK']"))
         )
+        browser.execute_script("arguments[0].scrollIntoView(true);", ok_button)
+        time.sleep(1)
         ok_button.click()
-
-        print('You successfully clocked-in')
+        
 
     except Exception as e:
         print(f"An error occurred: {e}")
     finally: 
-        time.sleep(5)  
+        time.sleep(5)  # Adjust as needed
         browser.quit()
-        print('You did it!')
+        print('You successfully clocked-in')
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: python clockin.py <Line Number>")
+        print("Usage: python script_name.py <Line Number>")
         sys.exit(1)
     try:
         position_line_number = int(sys.argv[1])
